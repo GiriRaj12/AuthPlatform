498407494f8a23056bbd9b4cfdd04828dfb7aece
Giri Raj <girirajsekar50@gmail.com>
2025-11-18 21:18:46 -0400

    Added basic sckeleton

---
+++ src/Utils/Redis.utils.js
@@ -0,0 +1,36 @@
+import {createClient, RedisClientType} from 'redis'
+
+
+export default class RedisHelper {
+
+    static REDIS_CLIENT = null;
+    REDIS_HOST = process.env.REDIS_HOST;
+    REDIS_PORT = process.env.REDIS_PORT;
+
+    constructor(){
+        if(!(this.REDIS_HOST && this.REDIS_PORT))
+            throw Error("Cannot connect to Redis host without host and port")
+
+        if(! RedisHelper.REDIS_CLIENT){
+            RedisHelper.REDIS_CLIENT = createClient({url: `redis://${this.REDIS_HOST}:${this.REDIS_PORT}`});
+
+            if(!RedisHelper.REDIS_CLIENT.isOpen)
+                RedisHelper.REDIS_CLIENT.connect()
+        }
+
+
+        RedisHelper.REDIS_CLIENT.on("connect", () => {console.log("Redis Connected !")});
+        RedisHelper.REDIS_CLIENT.on("ready", () => {console.log("Redis Connected and Accepting Data !")});
+        RedisHelper.REDIS_CLIENT.on("end", () => {console.log("Redis Disconnected !")});
+        RedisHelper.REDIS_CLIENT.on("error", () => {console.log("Redis Connection Error !")});
+    }
+
+    getValue(key){
+        RedisHelper.REDIS_CLIENT.
+    }
+
+    putValue(key, value){
+
+    }
+
+}
